f0275668f823f972c5f98bdcb4a8d55f896b9d28
Makkie <hortensia221974@yahoo.co.jp>
2019-07-09 16:41:55 +0900

    Setup smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,20 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "reunimos_#{Rails.env}"
 
+  #config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = true
+
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'production.rp27pkrsx5.ap-southeast-2.elasticbeanstalk.com', port: 80 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            'appdevelopment6619@gmail.com',
+    password:             'development2306',
+    authentication:       'plain',
+    enable_starttls_auto: true }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
